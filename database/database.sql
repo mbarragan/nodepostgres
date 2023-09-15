ede4bc5613bd4a3f8d661c8560a49fb305d82839
MANU <mbarragan@gmail.com>
2023-09-15 14:04:50 +0200

    create table pet

--- database/database.sql
+++ database/database.sql
@@ -1,1 +1,5 @@
--- SQL commands here
+-- Create a table 'pet' in schema 'demopets'
+CREATE TABLE IF NOT EXISTS demopets.pet (
+    id Serial NOT NULL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    owner_id bigint NOT NULL);
